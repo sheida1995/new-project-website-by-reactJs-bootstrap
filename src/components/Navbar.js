555e49ecfe22673854a135e5c7c5a19f10be7ee0
sheidaarvan <sheidaarvan1@gmail.com>
2023-10-17 02:12:12 +0330

    new project-website by reactJs & bootstrap

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+
+const Navbar = () => {
+    const [openHamburgerMenu , setOpenHamburgerMenu] = useState('');
+    const hamburgerToggle = () =>{
+        let hamburgerMenuDiv = document.getElementById('collapse');
+        hamburgerMenuDiv.classList.remove("collapse");
+        
+        setOpenHamburgerMenu(openHamburgerMenu)
+
+    }
+   
+
+  return (
+    <nav id='nav' className='navbar bg-dark navbar-dark navbar-expand-lg'>
+        <div className='container'>
+        <a href='' className='navbar-brand'>WEBSITE</a>
+        <button className='navbar-toggler' onClick={hamburgerToggle} > 
+            <span className='navbar-toggler-icon'></span>
+        </button>
+        <div id='collapse' className='collapse navbar-collapse'>
+            <ul className='navbar-nav'>
+                <li className='nav-item'>
+                    <a href='' className='nav-link'>Home</a>
+                </li>
+                <li className='nav-item'>
+                    <a href='' className='nav-link'>Explore</a>
+                </li>
+                <li className='nav-item'>
+                    <a href='' className='nav-link'>Create</a>
+                </li>
+                <li className='nav-item'>
+                    <a href='' className='nav-link'>Share</a>
+                </li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
